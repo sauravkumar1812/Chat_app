62a8762f2b103951f0997e88f9a0bec563127fa7
saurav1812 <shorya181299@gmail.com>
2024-05-24 22:55:28 +0530

    normal commit

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
- import {Container} from '@mui.material'
+import React, { useState } from "react";
+import { Container } from "@mui.material";
+
+const isLogin = () => {
+  const [isLogin, setIsLogin] = useState(true);
+};
+
 const Login = () => {
-  return  <Container component = {main} maxWidth="sx"> 
-  <paper elevation ={3} sx = {{padding:4 , display: "flex", flexDirection:"column",alignItems:"center"}}  >
+  return (
+    <Container component={main} maxWidth="sx">
+      <paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {isLogin ? <span>Login</span> : <span>Register</span>}
+      </paper>
+    </Container>
+  );
+};
 
-  </paper>
-  
-  </Container>
-}
-
-export default Login
+export default Login;
