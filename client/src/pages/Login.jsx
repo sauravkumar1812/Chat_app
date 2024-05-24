ada3df78d22d2dd60d6c07baddbad02f605b2512
saurav1812 <shorya181299@gmail.com>
2024-05-24 22:52:18 +0530

    normal commit

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+ import {Container} from '@mui.material'
+const Login = () => {
+  return  <Container component = {main} maxWidth="sx"> 
+  <paper elevation ={3} sx = {{padding:4 , display: "flex", flexDirection:"column",alignItems:"center"}}  >
+
+  </paper>
+  
+  </Container>
+}
+
+export default Login
