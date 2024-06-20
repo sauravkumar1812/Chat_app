e72843fd906ec99a7e60661819eacef0d0dc0a58
saurav1812 <shorya181299@gmail.com>
2024-06-20 16:18:30 +0530

    search box commit

--- client/src/components/specific/Search.jsx
+++ client/src/components/specific/Search.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Dialog, DialogTitle, InputAdornment, Stack, TextField } from "@mui/material";
+import { useInputValidation } from "6pp";
+import SearchIcon from "@mui/icons-material/Search";
 
 const SearchDialog = () => {
+  const search = useInputValidation("");
   return (
-    <div>search</div>
-  )
-}
+    <Dialog open>
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+        <TextField
+          label=""
+          value={search.value}
+          onChange={search.changeHandler}
+          variant="outlined"
+          size="small"
+          InputProps={{
+            startAdornment:(
+              <InputAdornment position="start">
+                <SearchIcon/>
+              </InputAdornment>
+            )
+          }}
+        />
+      </Stack>
+    </Dialog>
+  );
+};
 
-export default SearchDialog
+export default SearchDialog;
