f6386231564c359ba4d4ace4223ed84d8825ae3a
saurav1812 <shorya181299@gmail.com>
2024-06-20 13:26:49 +0530

    normal commit

--- client/src/components/specific/ChatList.jsx
+++ client/src/components/specific/ChatList.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Stack } from '@mui/material';
-import ChatItem from '../shared/ChatItem';
+import React from "react";
+import { Stack } from "@mui/material";
+import ChatItem from "../shared/ChatItem";
 const ChatList = ({
   width = "100%",
   chats = [],
@@ -11,16 +11,34 @@ const ChatList = ({
       chatId: "1",
       count: 0,
     },
-  ],handleDeleteChat,
+  ],
+  handleDeleteChat,
 }) => {
-  return <Stack width={width} direction={"column"}>
-    {
-        chats?.map((data)=>{
-            return <ChatItem/>;
-        })
-    }
-  </Stack>;
+  return (
+    <Stack width={width} direction={"column"}>
+      {chats?.map((data, index) => {
+        const { avatar, _id, name, groupChat, members } = data;
+        const newMessageAlert = newmessagesAlert.find(
+          ({ chatId }) => alert.chatId === _id
+        );
+        const isOnline = members?.some((member) => onlineusers.includes(_id));
+        return (
+          <ChatItem
+          index={index}
+            newMessageAlert={newMessageAlert}
+            isOnline={isOnline}
+            avatar={avatar}
+            name={name}
+            _id={id}
+            key={_id}
+            groupChat={groupChat}
+            sameSender={chatId === _id}
+            handleDeleteChatOpen={handleDeleteChat}
+          />
+        );
+      })}
+    </Stack>
+  );
 };
-
 
 export default ChatList;
