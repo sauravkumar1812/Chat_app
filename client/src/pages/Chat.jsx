2b94310ffa47d313d5ff0d97a35323dada8723d6
saurav1812 <shorya181299@gmail.com>
2024-06-21 15:12:55 +0530

    normal commit

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,39 +1,68 @@
-import React from 'react'
-import AppLayout from '../components/layout/AppLayout'
-import { Stack } from '@mui/material';
-import { grayColor } from '../constants/color';
-import { useRef } from 'react';
-import {IconButton} from '@mui/material';
-import { AttachFile as AttachFileIcon } from '@mui/icons-material';
+import React from "react";
+import AppLayout from "../components/layout/AppLayout";
+import { Stack } from "@mui/material";
+import { grayColor } from "../constants/color";
+import { useRef } from "react";
+import { IconButton } from "@mui/material";
+import {
+  AttachFile as AttachFileIcon,
+  Send as SendIcon,
+} from "@mui/icons-material";
+import { InputBox } from "../components/styles/styledComponents";
+import { orange } from "@mui/material/colors";
+
 const Chat = () => {
   const containerRef = useRef(null);
   return (
     <>
-    <Stack ref={containerRef}
-    
-    boxSizing={"border-box"}
-    padding={"1rem"}
-    spacing={"1rem"}
-    bgcolor={grayColor}
-    height={"90%"}
-    sx={{
-      overflowX:"hidden",
-      overflowY:"auto",
-    }}>
-
-
-</Stack>
-  <form style={{
-    height:"10%"
-  }}>
-    <Stack>
-      <IconButton>
-           <AttachFileIcon />
-      </IconButton>
-    </Stack>
-  </form>
+      <Stack
+        ref={containerRef}
+        boxSizing={"border-box"}
+        padding={"1rem"}
+        spacing={"1rem"}
+        bgcolor={grayColor}
+        height={"90%"}
+        sx={{
+          overflowX: "hidden",
+          overflowY: "auto",
+        }}
+      ></Stack>
+      <form
+        style={{
+          height: "10%",
+        }}
+      >
+        <Stack
+          direction={"row"}
+          height={"100%"}
+          width={"100%"}
+          padding={"1rem"}
+          alignItems={"center"}
+          position={"relative"}
+        >
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <InputBox placeholder="Type your messages here....." />
+          <IconButton
+            type="submit"
+            sx={{
+              rotate: "-30deg",
+              bgcolor: orange,
+              color: "white",
+              marginLeft: "1rem",
+              padding: "0.5rem",
+              "&:hover": {
+                bgcolor: "error.dark",
+              },
+            }}
+          >
+            <SendIcon />
+          </IconButton>
+        </Stack>
+      </form>
     </>
-  )
-}
+  );
+};
 
 export default AppLayout()(Chat);
