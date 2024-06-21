0fb8b4557873b0c188d7e716e2b052f37855030f
saurav1812 <shorya181299@gmail.com>
2024-06-21 14:44:35 +0530

    normal commmit

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import AppLayout from '../components/layout/AppLayout'
-
+import { Stack } from '@mui/material';
+import { grayColor } from '../constants/color';
+import { useRef } from 'react';
 
 const Chat = () => {
+  const containerRef = useRef(null);
   return (
-    <div>Chat</div>
+    <>
+    <Stack ref={containerRef}
+    
+    boxSizing={"border-box"}
+    padding={"1rem"}
+    spacing={"1rem"}
+    bgcolor={grayColor}
+    height={"90%"}
+    sx={{
+      overflowX:"hidden",
+      overflowY:"auto",
+    }}>
+
+
+</Stack>
+
+    </>
   )
 }
 
