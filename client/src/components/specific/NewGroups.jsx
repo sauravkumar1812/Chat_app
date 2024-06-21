da5e0b9d7b080e38ad19ad4782265ac933c5a98c
saurav1812 <shorya181299@gmail.com>
2024-06-21 12:53:59 +0530

    normal commit

--- client/src/components/specific/NewGroups.jsx
+++ client/src/components/specific/NewGroups.jsx
@@ -1,43 +1,39 @@
-import React from 'react'
-import React, { memo } from "react";
 import {
   Dialog,
   DialogTitle,
   Stack,
-  ListItem,
-  Typography,
-  Avatar,
   TextField,
+  Typography,
 } from "@mui/material";
-import {sampleUsers} from "../../constants/sampleData"
+import React from "react";
+import { sampleUsers } from "../../constants/sampleData";
+import UserItem from "../shared/UserItem";
 const NewGroupDialog = () => {
-  const selectMemberHandler = () => {
-
-  }
+  const selectMemberHandler = () => {};
   return (
     <Dialog open>
-    <Stack p={{ xs: "1rem", sm: "2rem" }} maxWidth={"25rem"}>
-      <DialogTitle>New group</DialogTitle>
-      <TextField/>
-      <Typography>
-        Members
-      </Typography>
-     <Stack>
-     {sampleUsers.map((user) => (
+      <Stack p={{ xs: "1rem", sm: "2rem" }} maxWidth={"25rem"}>
+        <DialogTitle>New group</DialogTitle>
+        <TextField />
+        <Typography>Members</Typography>
+        <Stack>
+          {sampleUsers.map((user) => (
             <UserItem
               user={user}
               key={user._id}
               handler={selectMemberHandler}
             />
           ))}
-     </Stack>
-     <Stack direction={"row"}>
-     <Button>Decline</Button>
-     <Button>Create</Button>
-     </Stack>
-    </Stack>
-  </Dialog>
-  )
-}
+        </Stack>
+        <Stack direction={"row"}>
+          <Button varaint="text" color="error">
+            Decline
+          </Button>
+          <Button varaint="contained">Create</Button>
+        </Stack>
+      </Stack>
+    </Dialog>
+  );
+};
 
-export default NewGroupDialog
+export default NewGroupDialog;
