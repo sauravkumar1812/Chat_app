9a02472a7ab63664e2bdba19f8ab66e080db74b1
saurav1812 <shorya181299@gmail.com>
2024-06-22 15:49:16 +0530

    normal commit

--- client/src/pages/Groups.jsx
+++ client/src/pages/Groups.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Grid } from "@mui/material";
+import React from "react";
 
 const Group = () => {
   return (
-    <div>Group</div>
-  )
-}
+    <Grid container height={"100vh"}>
+      <Grid
+        item
+        sx={{
+          display: {
+            xs: "none",
+            sm: "block",
+          },
+        }}
+        sm={4}
+      >
+        Group list
+      </Grid>
+      <Grid item xs={12}sm={8} sx={{
+        display:"flex",
+        flexDirection:"column",
+        alignItems:"center",
+        position:"relative",
+        padding:"1rem 3rem"
+      }}>
+        Group Details
+      </Grid>
+    </Grid>
+  );
+};
 
-export default Group
+export default Group;
