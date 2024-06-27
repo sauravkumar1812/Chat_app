d79725827cb246eb0041c511d5917013d72320c0
saurav1812 <shorya181299@gmail.com>
2024-06-27 15:49:09 +0530

    normal commit

--- client/src/components/specific/Charts.jsx
+++ client/src/components/specific/Charts.jsx
@@ -5,8 +5,20 @@ import  {Line,Doughnut} from 'react-chartjs-2'
 
 ChartJS.register();
 const LineChart = () => {
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            backgroundColor: 'rgba(75,192,192,0.2)',
+            borderColor: 'rgba(75,192,192,1)',
+        },
+    ],
+};
   return (
-   <Line data={{}}/>
+   <Line data={{data}}/>
   )
 }
 
