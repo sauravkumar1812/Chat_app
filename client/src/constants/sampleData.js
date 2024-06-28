2c3be271d3a3f972aa7a8187ee5849df447e7c9b
saurav1812 <shorya181299@gmail.com>
2024-06-28 12:52:31 +0530

    normal commit

--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -1,4 +1,3 @@
-
 export const samplechats = [
   {
     avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
@@ -17,99 +16,114 @@ export const samplechats = [
   },
 ];
 
-export  const sampleUsers=[{
-  avatar:["https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg"],
-  name:"Saurav kumar",
-  _id:"1",
-},
-{
-  avatar:["https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg"],
-      name:"Saurav singh",
-      _id:"2",
-}];
-
-
-export const sampleNotifications=[
+export const sampleUsers = [
   {
-    sender:{
-      avatar:["https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg"],
-    name:"Saurav kumar",
-    },
-    _id:"1",
+    avatar: [
+      "https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg",
+    ],
+    name: "Saurav kumar",
+    _id: "1",
   },
   {
-    sender:{
-      avatar:["https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg"],
-        name:"Saurav singh",
-    },
-        _id:"2",
-  }
-]
+    avatar: [
+      "https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg",
+    ],
+    name: "Saurav singh",
+    _id: "2",
+  },
+];
 
-export const sampleMessage=[
-    {
-      attachments: [
-        {
-          public_id: "asdsad 2",
-          url: "https://www.w3schools.com/howto/img_avatar.png",
-        },
+export const sampleNotifications = [
+  {
+    sender: {
+      avatar: [
+        "https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg",
       ],
-      content: "kiska ka Message hai",
-      _id: "sfnsdjkfsdnfkjsbnd",
-      sender: {
-        _id: "ssdfsdfsdf",
-        name: "Chaman ",
-      },
-      chat: "chatId",
-      createdAt: "2024-02-12T10:41:30.630Z",
+      name: "Saurav kumar",
     },
-  
-    {
-      attachments: [
-        {
-          public_id: "asdsad 2",
-          url: "https://www.w3schools.com/howto/img_avatar.png",
-        },
+    _id: "1",
+  },
+  {
+    sender: {
+      avatar: [
+        "https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg",
       ],
-      content: "hamko kya hi pata h bhai?",
-      _id: "sfnsdjkfsdnfkdbvjdddjsbnd",
-      sender: {
-        _id: "sdfsdfsdf",
-        name: "Chaman 2",
-      },
-      chat: "chatId",
-      createdAt: "2024-02-12T10:41:30.630Z",
+      name: "Saurav singh",
     },
-  
-]
+    _id: "2",
+  },
+];
 
+export const sampleMessage = [
+  {
+    attachments: [
+      {
+        public_id: "asdsad 2",
+        url: "https://www.w3schools.com/howto/img_avatar.png",
+      },
+    ],
+    content: "kiska ka Message hai",
+    _id: "sfnsdjkfsdnfkjsbnd",
+    sender: {
+      _id: "ssdfsdfsdf",
+      name: "Chaman ",
+    },
+    chat: "chatId",
+    createdAt: "2024-02-12T10:41:30.630Z",
+  },
+
+  {
+    attachments: [
+      {
+        public_id: "asdsad 2",
+        url: "https://www.w3schools.com/howto/img_avatar.png",
+      },
+    ],
+    content: "hamko kya hi pata h bhai?",
+    _id: "sfnsdjkfsdnfkdbvjdddjsbnd",
+    sender: {
+      _id: "sdfsdfsdf",
+      name: "Chaman 2",
+    },
+    chat: "chatId",
+    createdAt: "2024-02-12T10:41:30.630Z",
+  },
+];
 
 export const dashboardData = {
-  users:[{
-    avatar:["https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg"],
-    name:"Saurav kumar",
-    _id:"1",
-    username:"saurav",
-    friends:20,
-    groups:5
-  },
-  {
-    avatar:["https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg"],
-        name:"Saurav singh",
-        _id:"2",
-        username:"Shaurya",
-    friends:50,
-    groups:12
-  }],
+  users: [
+    {
+      avatar: [
+        "https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg",
+      ],
+      name: "Saurav kumar",
+      _id: "1",
+      username: "saurav",
+      friends: 20,
+      groups: 5,
+    },
+    {
+      avatar: [
+        "https://write.geeksforgeeks.org/static/media/Group%20210.08204759.svg",
+      ],
+      name: "Saurav singh",
+      _id: "2",
+      username: "Shaurya",
+      friends: 50,
+      groups: 12,
+    },
+  ],
 
-  chats:[
+  chats: [
     {
       avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
       name: "John Doe",
       _id: "1",
       groupChat: false,
-      members: [{_id:"1",avatar:"https://www.w3schools.com/howto/img_avatar.png"},
-        {_id:"2",avatar:"https://www.w3schools.com/howto/img_avatar.png"},],
+      members: [
+        { _id: "1", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+        { _id: "2", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+      ],
       totalMessages: 20,
       totalmembers: 2,
       creator: {
@@ -117,14 +131,16 @@ export const dashboardData = {
         avatar: "https://www.w3schools.com/howto/img_avatar.png",
       },
     },
-  
+
     {
       avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
       name: "The Weekend Wibes",
       _id: "2",
       groupChat: true,
-      members: [{_id:"1",avatar:"https://www.w3schools.com/howto/img_avatar.png"},
-        {_id:"2",avatar:"https://www.w3schools.com/howto/img_avatar.png"},],
+      members: [
+        { _id: "1", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+        { _id: "2", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+      ],
       totalMessages: 96,
       totalmembers: 12,
       creator: {
@@ -133,7 +149,7 @@ export const dashboardData = {
       },
     },
   ],
-  messages:[
+  messages: [
     {
       attachments: [
         {
@@ -144,13 +160,15 @@ export const dashboardData = {
       content: "kiska ka Message hai",
       _id: "sfnsdjkfsdnfkjsbnd",
       sender: {
-        _id: "ssdfsdfsdf",
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
         name: "Chaman ",
       },
+
       chat: "chatId",
+      groupchat: false,
       createdAt: "2024-02-12T10:41:30.630Z",
     },
-  
+
     {
       attachments: [
         {
@@ -161,11 +179,13 @@ export const dashboardData = {
       content: "hamko kya hi pata h bhai?",
       _id: "sfnsdjkfsdnfkdbvjdddjsbnd",
       sender: {
-        _id: "sdfsdfsdf",
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
         name: "Chaman 2",
       },
+
       chat: "chatId",
+      groupchat: true,
       createdAt: "2024-02-12T10:41:30.630Z",
     },
-  ]
-}
+  ],
+};
--- client/src/pages/admin/MessageManagment.jsx
+++ client/src/pages/admin/MessageManagment.jsx
@@ -1,6 +1,10 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import AdminLayout from '../../components/layout/AdminLayout'
 import Table from '../../components/shared/Table'
+import { transformImage } from '../../lib/features'
+import moment from 'moment'
+import { Avatar, Stack } from '@mui/material'
+import { dashboardData } from '../../constants/sampleData'
 
 const columns = [{
   field: 'id',
@@ -57,6 +61,16 @@ const columns = [{
 ]
 const MessageManagment = () => {
   const [rows, setRows] = useState([]);
+ 
+   useEffect(()=>{
+      setRows(dashboardData.messages.map(i=> ({...i,id:i._id,sender:{
+        name:i.sender.name,
+        avatar:transformImage(i.sender.avatar,50)
+      },
+       createdAt:moment(i.createdAt).format("MMMM Do YYYY, h:mm:ss a")  
+    
+    })))
+   },[])
 
   return (
     <AdminLayout>
