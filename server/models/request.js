32ef2418984b4dc8a3dc68a85844d0516ffc15b1
saurav1812 <shorya181299@gmail.com>
2024-06-29 12:34:25 +0530

    request Api Commit

--- server/models/request.js
+++ server/models/request.js
@@ -3,30 +3,22 @@ import { model,models,Schema } from "mongoose";
 
 
 const Schema = new Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    username:{
-        type:String,
-        required:true,
-        unquie:true
-    },  
-    password:{
-        type:String,
-        required:true,
-        select : false
-    },
-    avatar:{
-        public_id:{
-            type:String,
-            required:true
-        },
-        url:{   
-            type:String,
-            required:true
-        }
-    }
+    status: {
+        type: String,
+        default: "pending",
+        enum : ["pending","sent","delivered","read"]
+      },
+    sender: {
+        type: Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      receiver: {
+        type: Types.ObjectId,
+        ref: "Chat",
+        required: true,
+      },
+    
 },{timestamps:true});
 
 
