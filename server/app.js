83af0b37117c7e8a82adb3a432d6bb11ebfd5a0d
saurav1812 <shorya181299@gmail.com>
2024-07-03 15:08:43 +0530

    normal commit

--- server/app.js
+++ server/app.js
@@ -1,33 +1,32 @@
-import express from 'express';
-import userRoute from './routes/user.js';
-import {connectDB} from './utils/features.js';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import { errorMiddleware } from './middlewares/error.js';
-
+import express from "express";
+import { connectDB } from "./utils/features.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/error.js";
+import userRoute from "./routes/user.js";
+import chatRoute from './routes/chat.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cookieParser());
-dotenv.config({path:"./.env"});
-
+dotenv.config({ path: "./.env" });
 
 const mongoURI = process.env.MONGO_URI;
 
 const port = process.env.PORT || 3000;
 connectDB(mongoURI);
-app.use('/user', userRoute);
+
+app.use("/user", userRoute);
+app.use("/chat", chatRoute); 
 
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-    res.send('Hello World');
-    
-})
-
-app.use (errorMiddleware);
+app.use(errorMiddleware);
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    } ) ;
+  console.log(`Server is running on port ${port}`);
+});
---
+++ server/routes/chat.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { isAuthenticated } from "../middlewares/auth.js";
+
+const app = express.Router();
+
+app.use(isAuthenticated);
+
+
+
+export default app;
