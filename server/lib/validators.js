20ac4591b328192c46e4b9926eeb777020965c1a
saurav1812 <shorya181299@gmail.com>
2024-07-22 21:56:20 +0530

    minnor changes

--- server/lib/validators.js
+++ server/lib/validators.js
@@ -1,6 +1,18 @@
-import {body} from 'express-validator';
+import { body, validationResult } from "express-validator";
+const registerValidator = () => [
+  body("name","Please Enter Name").notEmpty(),
+  body("username","Please Enter Username").notEmpty(),
+  body("bio","Please Enter Bio").notEmpty(),
+  body("password","Please Enter Password").notEmpty(),
+];
+const validateHandler = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map((err) => err.msg)
+    console.log(errorMessages);
+    return res.status(400).json({ errors: errorMessages });
+  }
+   next();
+};
+export { registerValidator, validateHandler };
 
-const registerValidator = () => [body(["name", "username", "password","bio"]).notEmpty(),];
-
-
-export  { registerValidator }
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,12 +2,13 @@ import express from "express";
 import {getMyProfile, login, newUser,logout, searchUser} from "../controllers/user.js";
 import { singleAvatar } from "../middlewares/multer.js";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { registerValidator } from "../lib/validators.js";
+import { registerValidator, validateHandler } from "../lib/validators.js";
+
 
 const app = express.Router();
 
 
-app.post("/new",singleAvatar, registerValidator,newUser)
+app.post("/new",singleAvatar, registerValidator(),validateHandler,newUser)
 app.post("/login", login)
 
 
@@ -18,7 +19,6 @@ app.post("/login", login)
 app.use(isAuthenticated);
 app.get("/me",getMyProfile)
 app.get("/logout",logout)
-
 
 app.get("/search",searchUser)
 
