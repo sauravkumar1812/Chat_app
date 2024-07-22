3de12aa2daf9a414925fee92e4d7fcb84e8206a4
saurav1812 <shorya181299@gmail.com>
2024-07-22 23:32:52 +0530

    minnor commit

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
 import { compare } from "bcrypt";
 import { User } from "../models/User.js";
+import { Chat } from "../models/Chat.js";
 import { cookieOptions, sendToken } from "../utils/features.js";
 import { ErrorHandler } from "../utils/utility.js";
 import { TryCatch } from "../middlewares/error.js";
@@ -19,36 +20,35 @@ const newUser = async (req, res) => {
 };
 
 // Login user and save cookies use tryCatch as a middleware
-const login = async (req, res) => 
-  {
-    const { username, password } = req.body;
+const login = async (req, res) => {
+  const { username, password } = req.body;
 
-    // Ensure we await the user query
-    const user = await User.findOne({ username }).select("+password");
+  // Ensure we await the user query
+  const user = await User.findOne({ username }).select("+password");
 
-    // Check if the user exists
-    if (!user) {
-      return next(new ErrorHandler("Invalid Username or password", 404));
-    }
+  // Check if the user exists
+  if (!user) {
+    return next(new ErrorHandler("Invalid Username or password", 404));
+  }
 
-    // Compare the provided password with the hashed password in the database
-    const isMatch = await compare(password, user.password);
+  // Compare the provided password with the hashed password in the database
+  const isMatch = await compare(password, user.password);
 
-    // If passwords do not match
-    if (!isMatch) {
-      return next(new ErrorHandler("Invalid Username or password", 404));
-    }
+  // If passwords do not match
+  if (!isMatch) {
+    return next(new ErrorHandler("Invalid Username or password", 404));
+  }
 
-    // If passwords match, send token
-    sendToken(res, user, 200, `Welcome back, ${user.name}`);
-  };
+  // If passwords match, send token
+  sendToken(res, user, 200, `Welcome back, ${user.name}`);
+};
 
-  // Get the profile of the logged in user
+// Get the profile of the logged in user
 const getMyProfile = TryCatch(async (req, res) => {
   const user = await User.findById(req.user);
-    if(!user){
-        return next(new ErrorHandler("User not found",404))
-    }
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
   res.status(200).json({
     success: true,
     user,
@@ -57,22 +57,39 @@ const getMyProfile = TryCatch(async (req, res) => {
 
 // Logout user and clear cookies
 const logout = TryCatch(async (req, res) => {
-   
-    res.status(200).cookie("Chat-app-token","",{...cookieOptions,maxAge:0}).json({
+  res
+    .status(200)
+    .cookie("Chat-app-token", "", { ...cookieOptions, maxAge: 0 })
+    .json({
       success: true,
-      message:"Logged out successfully",
+      message: "Logged out successfully",
     });
-  });
-  
-  const searchUser = TryCatch(async (req, res) => {
-   
-    const {name} = req.query;
-   
+});
 
-
-    res.status(200).json({
-      success: true,
-      message:name,
-    });
+const searchUser = TryCatch(async (req, res) => {
+  const { name } = req.query;
+  const myChats = await Chat.find({
+    groupChat: false,
+    members: req.user,
   });
-export { login, newUser, getMyProfile,logout,searchUser };
+
+  //  All user from my chat means friends or people i have chated with
+  const allUsersFromMyChats = myChats.map((chat) => chat.members).flat();
+
+  //  for searching user except me and my friends using id and names
+  const allUsersExceptMeAndFriends = await User.find({
+    id: { $nin: allUsersFromMyChats },
+    name: { $regex: name, $options: "i" },
+  });
+
+  const users = allUsersExceptMeAndFriends.map(({ _id, name, avatar }) => ({
+    _id,
+    name,
+    avatar: avatar.url,
+  }));
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+export { login, newUser, getMyProfile, logout, searchUser };
