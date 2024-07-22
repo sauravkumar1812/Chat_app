1918ca6c453ad22cd35cb484cd0a9bf946486a19
saurav1812 <shorya181299@gmail.com>
2024-07-22 22:20:31 +0530

    user validator done

--- server/lib/validators.js
+++ server/lib/validators.js
@@ -1,18 +1,39 @@
-import { body, validationResult } from "express-validator";
-const registerValidator = () => [
-  body("name","Please Enter Name").notEmpty(),
-  body("username","Please Enter Username").notEmpty(),
-  body("bio","Please Enter Bio").notEmpty(),
-  body("password","Please Enter Password").notEmpty(),
-];
+import { body, validationResult,check } from "express-validator";
+import { ErrorHandler } from "../utils/utility.js";
+
+
+// Validate the request body
 const validateHandler = (req, res, next) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const errorMessages = errors.array().map((err) => err.msg)
-    console.log(errorMessages);
-    return res.status(400).json({ errors: errorMessages });
-  }
-   next();
+  const errorMessages = errors.array().map((err) => err.msg).join(",")
+  if (errors.isEmpty())  return next();
+  else next(new ErrorHandler(errorMessages,400))
+  
 };
-export { registerValidator, validateHandler };
+// Register Validator
+const registerValidator = () => [
+    body("name","Please Enter Name").notEmpty(),
+    body("username","Please Enter Username").notEmpty(),
+    body("bio","Please Enter Bio").notEmpty(),
+    body("password","Please Enter Password").notEmpty(),
+    check("avatar","Please Upload Avatar").notEmpty(),
+  ];
+
+//   Login Validator
+const loginValidator = () => [
+    body("username","Please Enter Username").notEmpty(),
+    body("password","Please Enter Password").notEmpty(),
+  ];
+
+//   
+const profileValidator = () => [
+    body("name","Please Enter Name").notEmpty(),
+    body("username","Please Enter Username").notEmpty(),
+    body("bio","Please Enter Bio").notEmpty(),
+    body("password","Please Enter Password").notEmpty(),
+    check("avatar","Please Upload Avatar").notEmpty(),
+  ];
+
+
+    export { registerValidator, validateHandler,loginValidator,profileValidator };
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,14 +2,14 @@ import express from "express";
 import {getMyProfile, login, newUser,logout, searchUser} from "../controllers/user.js";
 import { singleAvatar } from "../middlewares/multer.js";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { registerValidator, validateHandler } from "../lib/validators.js";
+import { loginValidator, registerValidator, validateHandler } from "../lib/validators.js";
 
 
 const app = express.Router();
 
 
 app.post("/new",singleAvatar, registerValidator(),validateHandler,newUser)
-app.post("/login", login)
+app.post("/login",loginValidator(),validateHandler,login)
 
 
 
