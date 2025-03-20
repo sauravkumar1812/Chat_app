28708d88e3999fa12caec769e5f29e045bb9b481
sauravkumar1812 <shorya181299@gmail.com>
2025-03-21 00:54:03 +0530

    normal commit

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,15 +1,16 @@
 const corsOptions = {
-    origin: [
-       "https://saurav-chat-app-frontend.onrender.com",
-       "https://chat-app-7icu.onrender.com",
-      "http://localhost:5173",
-      "http://localhost:4173",
-      process.env.CLIENT_URL,
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  };
-  
-  const CHATTU_TOKEN = "Chat-app-token";
-  
-  export { corsOptions, CHATTU_TOKEN };
+  origin: [
+    "https://saurav-chat-app-frontend.onrender.com",
+    "https://chat-app-7icu.onrender.com",
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+  ].filter(Boolean), // Removes undefined values
+
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+const CHATTU_TOKEN = "Chat-app-token";
+
+export { corsOptions, CHATTU_TOKEN };
